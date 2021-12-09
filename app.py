877386863bb6b399d384f15d995c214dad0315fb
Gilvan Almeida <gilvan.praxedes@outlook.com>
2021-12-09 00:32:00 -0300

    Fixing words

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/')
 def index():
-    website_data = yaml.load(open('_config.yaml', encoding='utf8'))
+    website_data = yaml.safe_load(open('_config.yaml', encoding='utf8'))
 
     return render_template('index.html', data=website_data)
 
 
 @app.route('/pt-br')
 def version_pt_br():
-    website_data = yaml.load(open('_config-pt-br.yaml', encoding='utf8'))
+    website_data = yaml.safe_load(open('_config-pt-br.yaml', encoding='utf8'))
 
     return render_template('index.html', data=website_data, language='pt-br')
 
